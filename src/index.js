bed55b7fb1ec2285f597b1821b66eb29bf2784dc
沐云湘夕 <zqc4073@outlook.com>
2023-01-08 23:17:50 +0800

    加入对官方 V3 版本支持，修复部分提示内容

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = (ctx) => {
     const register = () => {
         ctx.helper.uploader.register(UPLOADER, {
             handle,
-            name: UPLOADER,
+            name: 'Chevereto V4 Uploader',
             config: config
         })
     }
@@ -16,18 +16,16 @@ module.exports = (ctx) => {
             let imgList = ctx.output;
             for (let i in imgList) {
                 let image = imgList[i].buffer;
+                let name = imgList[i].fileName;
                 if (!image && imgList[i].base64Image) {
                     image = Buffer.from(imgList[i].base64Image, 'base64');
                 }
-                const postConfig = postOptions(
+                let postConfig = postOptions(
                     image,
-                    imgList[i].fileName,
-                    userConfig.url,
-                    userConfig.key,
-                    userConfig.source_param || 'source',
-                    userConfig.album_id,
-                    userConfig.category_id
+                    name,
+                    userConfig
                 );
+                console.log(postConfig);
                 let body = await ctx.request(postConfig);
                 if (!body) {
                     throw new Error('上传图片失败' + body);
@@ -55,23 +53,39 @@ module.exports = (ctx) => {
         }
     }
 
-    const postOptions = (image, fileName, url, key, source_param, album_id, category_id) => {
-        let headers = {
-            'contentType': 'multipart/form-data',
-            'User-Agent': 'PicGo',
-            'X-API-Key': key,
-        }
-        let formData = {
-            'format': 'json',
-            'album_id': album_id,
-            'category_id': category_id,
+    const postOptions = (image, fileName, userConfig) => {
+        let version = userConfig.cheveretoversion;
+        let key = userConfig.key;
+        let url = userConfig.url;
+        let source_param = userConfig.source_param || 'source';
+        let headers, formData = {};
+        if (version === 'V4') {
+            headers = {
+                'contentType': 'multipart/form-data',
+                'User-Agent': 'PicGo',
+                'X-API-Key': key,
+            }
+            formData = {
+                'format': 'json',
+                'album_id': userConfig.album_id,
+                'category_id': userConfig.category_id,
+            }
+        } else {
+            headers = {
+                'contentType': 'multipart/form-data',
+                'User-Agent': 'PicGo',
+            }
+            formData = {
+                'format': 'json',
+                'key': key,
+            }
         }
         const opts = {
             method: 'POST',
             url: url,
             strictSSL: false,
             headers: headers,
-            formData: formData
+            formData: formData,
         }
         opts.formData[source_param] = {};
         opts.formData[source_param].value = image;
@@ -88,51 +102,69 @@ module.exports = (ctx) => {
         }
         return [
             {
+                name: 'cheveretoversion',
+                type: 'list',
+                default: userConfig.cheveretoversion || 'V3',
+                message: '选择 Chevereto 的图床版本',
+                choices: [
+                    {
+                        name: 'V3',
+                        value: 'V3'
+                    },
+                    {
+                        name: 'V4',
+                        value: 'V4'
+                    }
+                ],
+                required: true,
+                alias: 'Chevereto Version'
+            },
+            {
                 name: 'url',
                 type: 'input',
                 default: userConfig.url,
+                message: '使用 Chevereto 的图床的API上传网址（如：https://example.com/api/1/upload）',
                 required: true,
-                message: '使用 Chevereto V4 的图床的API上传网址（如：https://example.com/api/1/upload）',
                 alias: 'Url'
             },
             {
                 name: 'key',
                 type: 'input',
                 default: userConfig.key,
+                message: '对应版本的 Chevereto API KEY',
                 required: true,
-                message: '在个人设置页面获取的 API Key 或者是图床的 Public API key',
                 alias: 'API Key'
             },
             {
                 name: 'album_id',
                 type: 'input',
                 default: userConfig.album_id,
+                message: '（V4 版本）用户所拥有的相册 ID（参数为相册详细信息中的 id_encoded，默认不添加）',
                 required: false,
-                message: '用户所拥有的相册 ID（参数为相册详细信息中的 id_encoded，默认不添加）',
                 alias: 'Album ID'
             },
             {
                 name: 'category_id',
                 type: 'input',
                 default: userConfig.category_id,
+                message: '（V4 版本）图床的图片分类 ID （参数为整数，默认不添加）',
                 required: false,
-                message: '图床的图片分类 ID （参数为整数，默认不添加）',
                 alias: 'Category ID'
             },
             {
                 name: 'source_param',
                 type: 'input',
                 default: userConfig.source_param,
+                message: '上传API的文件参数（参数可不填，默认为source）',
                 required: false,
-                message: '上传API的文件参数（可不填，默认为source）',
                 alias: 'Source Param'
             },
             {
                 name: 'url_param',
                 type: 'input',
                 default: userConfig.url_param,
+                message: '获取返回图片链接的键名（参数可不填，默认为url）',
                 required: false,
-                message: '获取返回图片链接的键名（可不填，默认为url）',
                 alias: 'Url Param'
             }
         ]
