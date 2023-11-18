898653fe2443b41e797dc3a660bd7a2af27f38a7
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2023-11-18 13:45:34 +0800

    Fix bugs and use axios without picgo.request. Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,174 +1,158 @@
+const { default: axios } = require('axios')
+const FormData = require('form-data')
+
 module.exports = (ctx) => {
-    const UPLOADER = 'cheveretov4';
-    const register = () => {
-        ctx.helper.uploader.register(UPLOADER, {
-            handle,
-            name: 'Chevereto V4 Uploader',
-            config: config
-        })
-    }
+  const UPLOADER = 'cheveretov4'
+  const register = () => {
+    ctx.helper.uploader.register(UPLOADER, {
+      handle,
+      name: 'Chevereto V4 Uploader',
+      config
+    })
+  }
 
-    const postOptions = (image, fileName, userConfig) => {
-        const version = userConfig.cheveretoversion === "V4";
-        const key = userConfig.key;
-        const url = userConfig.url;
-        let V3headers = {
-            'contentType': 'multipart/form-data',
-            'User-Agent': 'PicGo',
-        }
-        let V3formData = {
-            'format': 'json',
-            'key': key,
-        }
-        let V4headers = {
-            'contentType': 'multipart/form-data',
-            'User-Agent': 'PicGo',
-            'X-API-Key': key,
-        }
-        let V4formData = {
-            'format': 'json',
-            'album_id': userConfig.album_id,
-            'category_id': userConfig.category_id,
-            'description':userConfig.description,
-        }
-        if (!userConfig.album_id) {
-            delete V4formData.album_id
-        }
-        if (!userConfig.category_id) {
-            delete V4formData.category_id
-        }
-        if (!userConfig.description) {
-            delete V4formData.description
-        }
-        const opts = {
-            method: 'POST',
-            url: url,
-            strictSSL: false,
-            headers: version ? V4headers : V3headers,
-            formData: version ? V4formData : V3formData,
-        }
-        opts.formData['source'] = {};
-        opts.formData['source'].value = image;
-        opts.formData['source'].options = {
-            filename: fileName
-        }
-        return opts;
+  const postOptions = (image, fileName, userConfig) => {
+    const version = userConfig.cheveretoversion === 'V4'
+    const key = userConfig.key
+    const url = userConfig.url
+    const headers = {
+      'Content-Type': 'multipart/form-data',
+      'User-Agent': 'PicGo',
+      Connection: 'keep-alive',
+      Accept: 'application/json'
     }
+    const data = new FormData()
+    data.append('source', image, fileName)
+    data.append('format', 'json')
+    if (version) {
+      headers['X-API-Key'] = key
+      if (userConfig.album_id) {
+        data.append('album_id', userConfig.album_id)
+      }
+      if (userConfig.category_id) {
+        data.append('category_id', userConfig.category_id)
+      }
+      if (userConfig.description) {
+        data.append('description', userConfig.description)
+      }
+    }
+    if (!version) {
+      data.key = key
+    }
+    const opts = {
+      method: 'POST',
+      url,
+      headers,
+      data
+    }
+    return opts
+  }
 
-    const handle = async function (ctx) {
-        let userConfig = ctx.getConfig('picBed.' + UPLOADER);
-        if (!userConfig) {
-            throw new Error('Can\'t find uploader config');
-        }
-        try {
-            const imgList = ctx.output;
-            for (let i in imgList) {
-                const image = imgList[i].buffer;
-                const name = imgList[i].fileName;
-                if (!image && imgList[i].base64Image) {
-                    image = Buffer.from(imgList[i].base64Image, 'base64');
-                }
-                const postConfig = postOptions(
-                    image,
-                    name,
-                    userConfig
-                );
-                let body = await ctx.request(postConfig);
-                if (!body) {
-                    throw new Error('上传图片失败' + body);
-                }
-                delete imgList[i].base64Image;
-                delete imgList[i].buffer;
-                body = JSON.parse(body);
-                if (body['status_code'] == 200) {
-                    imgList[i]['imgUrl'] = eval('body.image.url');
-                } else {
-                    ctx.emit('notification', {
-                        title: '上传失败',
-                        body: body.status_txt
-                    })
-                    throw new Error('上传失败' + body);
-                }
-            }
-            console.log(imgList);
-        } catch (err) {
-            ctx.emit('notification', {
-                title: '上传失败',
-                body: '请检查服务端或配置'
-            })
-            throw err;
-        }
+  const handle = async function (ctx) {
+    const userConfig = ctx.getConfig('picBed.' + UPLOADER)
+    if (!userConfig) {
+      throw new Error('Can\'t find uploader config')
     }
+    try {
+      const imgList = ctx.output
+      for (const i in imgList) {
+        if (!imgList[i].buffer && !imgList[i].base64Image) {
+          continue
+        }
+        const image = imgList[i].buffer || Buffer.from(imgList[i].base64Image, 'base64')
+        const fileName = imgList[i].fileName
+        const postConfig = postOptions(
+          image,
+          fileName,
+          userConfig
+        )
+        const body = await axios(postConfig)
+        delete imgList[i].base64Image
+        delete imgList[i].buffer
+        if (body.data.status_code === 200) {
+          imgList[i].imgUrl = body.data.image.url
+        } else {
+          throw new Error('上传失败' + body || body.data)
+        }
+      }
+    } catch (err) {
+      ctx.emit('notification', {
+        title: '上传失败',
+        body: `错误信息：${err.message || err}`
+      })
+      throw new Error(err.message || err)
+    }
+  }
 
-    const config = ctx => {
-        let userConfig = ctx.getConfig('picBed.' + UPLOADER);
-        if (!userConfig) {
-            userConfig = {}
-        }
-        return [
-            {
-                name: 'cheveretoversion',
-                type: 'list',
-                default: userConfig.cheveretoversion || 'V3',
-                message: '选择 Chevereto 的图床版本',
-                choices: [
-                    {
-                        name: 'V3',
-                        value: 'V3'
-                    },
-                    {
-                        name: 'V4',
-                        value: 'V4'
-                    }
-                ],
-                required: true,
-                alias: 'Chevereto Version'
-            },
-            {
-                name: 'url',
-                type: 'input',
-                default: userConfig.url,
-                message: '使用 Chevereto 的图床的API上传网址（如：https://example.com/api/1/upload）',
-                required: true,
-                alias: 'Url'
-            },
-            {
-                name: 'key',
-                type: 'input',
-                default: userConfig.key,
-                message: '对应版本的 Chevereto API KEY',
-                required: true,
-                alias: 'API Key'
-            },
-            {
-                name: 'album_id',
-                type: 'input',
-                default: userConfig.album_id,
-                message: '（V4 版本）用户所拥有的相册 ID（参数为相册详细信息中的 id_encoded，默认不添加）',
-                required: false,
-                alias: 'Album ID'
-            },
-            {
-                name: 'category_id',
-                type: 'input',
-                default: userConfig.category_id,
-                message: '（V4 版本）图床的图片分类 ID （参数为整数，默认不添加）',
-                required: false,
-                alias: 'Category ID'
-            },
-            {
-                name: 'description',
-                type: 'input',
-                default: userConfig.description,
-                message: '图片描述（默认不添加）',
-                required: false,
-                alias: 'Description'
-            }
-        ]
+  const config = ctx => {
+    let userConfig = ctx.getConfig('picBed.' + UPLOADER)
+    if (!userConfig) {
+      userConfig = {}
     }
+    return [
+      {
+        name: 'cheveretoversion',
+        type: 'list',
+        default: userConfig.cheveretoversion || 'V3',
+        message: '选择 Chevereto 的图床版本',
+        choices: [
+          {
+            name: 'V3',
+            value: 'V3'
+          },
+          {
+            name: 'V4',
+            value: 'V4'
+          }
+        ],
+        required: true,
+        alias: 'Chevereto Version'
+      },
+      {
+        name: 'url',
+        type: 'input',
+        default: userConfig.url,
+        message: '使用 Chevereto 的图床的API上传网址（如：https://example.com/api/1/upload）',
+        required: true,
+        alias: 'Url'
+      },
+      {
+        name: 'key',
+        type: 'input',
+        default: userConfig.key,
+        message: '对应版本的 Chevereto API KEY',
+        required: true,
+        alias: 'API Key'
+      },
+      {
+        name: 'album_id',
+        type: 'input',
+        default: userConfig.album_id,
+        message: '（V4 版本）用户所拥有的相册 ID（参数为相册详细信息中的 id_encoded，默认不添加）',
+        required: false,
+        alias: 'Album ID'
+      },
+      {
+        name: 'category_id',
+        type: 'input',
+        default: userConfig.category_id,
+        message: '（V4 版本）图床的图片分类 ID （参数为整数，默认不添加）',
+        required: false,
+        alias: 'Category ID'
+      },
+      {
+        name: 'description',
+        type: 'input',
+        default: userConfig.description,
+        message: '图片描述（默认不添加）',
+        required: false,
+        alias: 'Description'
+      }
+    ]
+  }
 
-    return {
-        uploader: UPLOADER,
-        register
-    }
+  return {
+    uploader: UPLOADER,
+    register
+  }
 }
